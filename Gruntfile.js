24ca52fe266480525df27f3fbd6ae08d77cd093e
llucaso <lredzinski@gmail.com>
2017-05-01 12:21:13 +0200

    header and background

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,47 +2,66 @@
 
   // Project configuration.
   grunt.initConfig({
-  	sass: {
-  		options: {
-  			sourceMap: true
-  		},
-  		dist: {
-  			files: {
-  				'css/style.css': 'sass/style.sass'
-  			}
-  		}
-  	},
+    sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'css/main.css': 'sass/main.sass'
+            }
+        }
+    },
 
-    browserSync: {
-            bsFiles: {
-                src: ['style/*.css', 'index.html']
-            },
-            options: {
-                server: {
-                    baseDir: "./"
-          }
+imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/img_ready/'
+        }]
       }
     },
 
-  watch: {
-    scripts: {
-        files: ['sass/*.sass'],
-        tasks: ['sass'],
-        options: {
-            spawn: false,
-        },
-    } 
-  },
+
+
+    watch: {
+        scripts: {
+            files: ['sass/*.sass'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },
+        }
+    },
+
+    browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
 
 
   });
-  // Load the plugins tasks 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  
-  grunt.registerTask('default', ['sass', 'watch']);
-  grunt.registerTask('sync', ['browserSync']);
+  //Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+
+  //Default task(s).
+  grunt.registerTask('default', ['sass','imagemin', 'browserSync', 'watch']);
+
 };
