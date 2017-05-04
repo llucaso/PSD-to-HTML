e525ecaa07f08c4cc39a97d585674bbe71d3de32
llucaso <lredzinski@gmail.com>
2017-05-04 12:56:36 +0200

    code update

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,16 +13,7 @@
         }
     },
 
-imagemin: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: ['**/*.{png,jpg,gif}'],
-          dest: 'images/img_ready/'
-        }]
-      }
-    },
+
 
 
 
@@ -56,12 +47,12 @@ imagemin: {
 
   //Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
 
 
   //Default task(s).
-  grunt.registerTask('default', ['sass','imagemin', 'browserSync', 'watch']);
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
 
 };
